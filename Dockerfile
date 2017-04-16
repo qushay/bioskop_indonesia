072131c2241dea31ae14eacbbbd5ba362321c9d4
qushay <bagas.qushay@gmail.com>
2017-04-17 06:03:00 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:latest
+MAINTAINER Qushay Bagas "bagas.qushay@gmail.com"
+
+RUN apt-get update
+# Install pip & selenium
+RUN apt-get install python-pip
+RUN pip install selenium
+# Install Required Packages
+RUN apt-get install build-essential chrpath libssl-dev libxft-dev
+RUN apt-get install libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev
+# Install PhantomJS
+RUN wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+RUN tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
+# create a soft link phantomjs binary file to systems bin dirctory
+RUN ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
+
+ADD now.py /home/now.py
+ADD soon.py /home/soon.py
+
+WORKDIR /home
---
+++ now.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+class runSelenium(object):
+    def __init__(self):
+        # define a class attribute
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(1120, 550)
+
+    def isElementPresent(self, locator):
+        try:
+            self.driver.find_element_by_xpath(locator)
+        except NoSuchElementException:
+            return False
+        return True
+    
+    def getElementData(self):
+        html_list = self.driver.find_element_by_id("mvlist")
+        movies = html_list.find_elements_by_tag_name("li")
+        for result in movies:
+            movie = result.find_element_by_xpath('.//div/h3/a')
+            title = movie.text
+            url = movie.get_attribute('href')
+            print("{} ({})".format(title, url))
+        self.goNextPage()
+
+    def goNextPage(self):
+        pagination = self.driver.find_element_by_id("top_pager")
+        isElement = self.isElementPresent("//*[contains(text(), 'Next >>')]")
+        if isElement:
+            next_page = pagination.find_element_by_xpath("//a[contains(text(), 'Next >>')]")
+            next_page.click()
+            self.getElementData()   
+
+    def selenium(self):
+        self.driver.get("http://www.21cineplex.com/nowplaying")
+        link_class = self.driver.find_element_by_class_name("mn2")
+        link = link_class.find_element_by_xpath('.//a')
+        link.click()
+
+        self.getElementData()
+        self.driver.quit()
+
+if __name__ == '__main__':
+    run = runSelenium()
+    run.selenium()
